6207a2622f2d36558853d50f079ef916d84a3e18
Jonathon Vandezande <jevandezande@gmail.com>
2020-09-23 13:32:52 -0400

    Adds initial setup.

--- README.rst
+++ README.rst
@@ -1,10 +1,12 @@
-# Cluster Calculation Code
+Cluster
+=======
 
 Generates clusters for X-ray computations following the model of Maganas et al.
 (DOI: 10.1039/c3cp50709b).
 
 
-## Description
+Description
+===========
 
 Generates clusters that are separated into three regions: qc (where quantum
 chemical methods are applied), br (which includes capped ECPs and point
@@ -12,6 +14,8 @@ charges), and pc (point charges). Currently only supports ORCA (PRs with
 support for other codes would be greatly appreciated).
 
 
-## Requirements
+Requirements
+============
 
 * cclib - reading of output files
+* jobqueue - submitting jobs
--- setup.py
+++ setup.py
@@ -1,17 +1,46 @@
 #!/usr/bin/env python3
-"""
-    Setup file for cluster.
-"""
 
-import sys
-from setuptools import setup
+"""The setup script."""
 
+from setuptools import setup, find_packages
 
-def setup_package():
-    needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
-    sphinx = ['sphinx'] if needs_sphinx else []
-    setup(setup_requires=sphinx)
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
 
-if __name__ == "__main__":
-    setup_package()
+requirements = []
+
+setup_requirements = ['pytest-runner']
+
+test_requirements = ['pytest>=3']
+
+setup(
+    author='Jonathon Vandezande',
+    author_email='jevandezande@gmail.com',
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description='Generates clusters for X-ray computations following the model of Maganas et al. (DOI: 10.1039/c3cp50709b).',
+    install_requires=requirements,
+    license='MIT license',
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='spectra',
+    name='spectra',
+    packages=find_packages(include=['cluster', 'cluster.*']),
+    scripts=[],
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/jevandezande/cluster',
+    version='0.1.0',
+    zip_safe=False,
+)
